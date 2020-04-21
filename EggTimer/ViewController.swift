f2bfc2d1c76acb274cabe6947a57cefe04653d6f
Javier Heisekce <javierheiseckee@gmail.com>
2020-04-21 10:22:41 -0400

    Se agrega accion de boton para cada huevo

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        if let text = sender.currentTitle {
+            switch text {
+            case "Hard":
+                print(12)
+            case "Medium":
+                print(7)
+            case "Soft":
+                print(5)
+            default:
+                print("Option not valid!")
+            }
+        }
+    }
 }
